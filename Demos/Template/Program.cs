7e8b18df325c984a3aba24f84e3b1421c40730ad
Kevin Griffin <kevin@kevgriffin.com>
2022-06-24 16:20:39 -0400

    Template

---
+++ Demos/Template/Program.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.Data.SqlClient;
+
+var connectionString = "Server=localhost/DEMOS;Database=DapperMastery;Trusted_Connection=True;";
+
+await using var connection = new SqlConnection(connectionString);
