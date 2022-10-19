dfde762fa0a1adc576277a26ac7ac8f4d9bd9b9e
Kevin Griffin <kevin@kevgriffin.com>
2022-10-19 15:48:33 -0400

    Add Query examples

---
+++ Demos/02 - Query/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Dapper;
+
+var connectionString = "Server=localhost\\DEMOS;Database=DapperMastery;Trusted_Connection=true;TrustServerCertificate=True";
+
+await using var connection = new SqlConnection(connectionString);
+
+var sql = @"SELECT [Id]
+      ,[FirstName]
+      ,[LastName]
+      ,[PasswordHash]
+      ,[DateOfBirth]
+      ,[CreatedDate]
+      ,[LastUpdated]
+  FROM [dbo].[ApplicationUsers]";
+
+var users = await connection.QueryAsync<ApplicationUser>(sql);
+
+// spit out result
+foreach (var user in users)
+    Console.WriteLine($"{user.FirstName} was born on {user.DateOfBirth:d}");
